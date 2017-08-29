f2e099d0bbb843c74e4ffeb838ee5f7b3c0b3d48
napnap75 <thomas@nappez.com>
2017-08-29 18:29:22 +0200

    Update build.sh

--- influxdb/build.sh
+++ influxdb/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 git clone https://github.com/influxdata/influxdata-docker
 cd influxdb/$1
 curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz && tar zx -f qemu-arm-static.tar.gz -C ${IMAGE}
