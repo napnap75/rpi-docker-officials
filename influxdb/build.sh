f46eb09872b348555ba3c3b926f5e0514af510b0
napnap75 <thomas@nappez.com>
2017-08-29 18:47:02 +0200

    Update build.sh

--- influxdb/build.sh
+++ influxdb/build.sh
@@ -7,5 +7,7 @@ cd influxdata-docker/influxdb/$1
 curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz \
   && tar zx -f qemu-arm-static.tar.gz
 patch Dockerfile ../../../Dockerfile_influxdb.patch
+sed sed -i.bak 's/amd64/linux_armhf/g' Dockerfile
+cat Dockerfile
 docker build -t napnap75/rpi-influxdb:$1 .
 docker images
