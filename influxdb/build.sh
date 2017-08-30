464c4b2534e36a91ca0004f93660b0c6a72646cf
napnap75 <thomas@nappez.com>
2017-08-30 10:39:57 +0200

    Now using Travis to build the images

---
+++ influxdb/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Make sure the script stops after an error
+set -e
+
+# Load the official source and update it for the arm processors
+git clone https://github.com/influxdata/influxdata-docker
+if [ "$2" == "" ]; then
+  cd influxdata-docker/influxdb/$1
+  patch Dockerfile ../../../Dockerfile_influxdb.patch
+else
+  cd influxdata-docker/influxdb/$1/$2
+  patch Dockerfile ../../../../Dockerfile_influxdb.patch
+fi
+sed -i.bak 's/-static_linux_amd64/_linux_armhf/g' Dockerfile
+cat Dockerfile
+
+# Add QEmu to allow the image to be built and tested on x86 processors (especially Travis CI)
+curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz \
+  && tar zx -f qemu-arm-static.tar.gz
+
+# Build the image
+if [ "$2" == "" ]; then
+  docker build -t napnap75/rpi-influxdb:$1 .
+else
+  docker build -t napnap75/rpi-influxdb:$1-$2 .
+fi
+docker images
--- jenkinsci/README.md
+++
@@ -1,8 +0,0 @@
-This is a clone of [the official Jenkins images](https://hub.docker.com/_/jenkins/) for the raspberry pi.
-
-The changes from the original images are :
-- built from napnap75/rpi-openjdk-official instead of openjdk
-
-The source code is available [here](https://github.com/napnap75/rpi-docker-officials/tree/master/jenkinsci) and the image [here](https://hub.docker.com/r/napnap75/rpi-jenkins-official/).
-
-This image is build every week.
