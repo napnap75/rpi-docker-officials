e3b436cd8bf3f4ad63a6369089644c3bba525993
napnap75 <thomas@nappez.com>
2017-11-07 16:01:35 +0100

    Update build.sh

--- jeedom/build.sh
+++ jeedom/build.sh
@@ -3,17 +3,25 @@
 # Make sure the script stops after every error and print the command launched
 set -ex
 
-# Load the official source and update it for the arm processors
-if [ -d core ] ; then rm -fr core ; fi
-git clone https://github.com/jeedom/core
-cd core
-patch Dockerfile ../Dockerfile.patch
-cat Dockerfile
+if [ "$2" == "" ]; then
+  # Load the official source and update it for the arm processors
+  if [ -d core ] ; then rm -fr core ; fi
+  git clone https://github.com/jeedom/core
+  cd core
+  patch Dockerfile ../Dockerfile.patch
+  cat Dockerfile
 
-# Add QEmu to allow the image to be built and tested on x86 processors (especially Travis CI)
-curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz \
-  && tar zx -f qemu-arm-static.tar.gz
+  # Add QEmu to allow the image to be built and tested on x86 processors (especially Travis CI)
+  curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz \
+    && tar zx -f qemu-arm-static.tar.gz
 
-# Build the image
-docker build -t napnap75/rpi-jeedom:latest .
+  # Build the image
+  docker build -t napnap75/rpi-jeedom:latest .
+elif [ "$2" == "openzwave" ]; then
+  cd openzwave
+  
+  # Build the image
+  docker build -t napnap75/rpi-jeedom:openzwave .
+fi
+
 docker images
