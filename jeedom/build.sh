55bb9d0861e9d2610ddb79f4b29048ab57a30912
napnap75 <thomas@nappez.com>
2018-06-05 15:22:54 +0200

    Moved Jeedom to the stable and packaged version (#14)

--- jeedom/build.sh
+++ jeedom/build.sh
@@ -6,14 +6,12 @@ set -ex
 if [ "$1" == "latest" ]; then
   # Load the official source and update it for the arm processors
   if [ -d core ] ; then rm -fr core ; fi
-  git clone -b stable https://github.com/jeedom/core
-  cd core
+  while [ "$DOWNLOAD_URL" == "" ] ; do DOWNLOAD_URL=$(curl -s https://api.github.com/repos/jeedom/core/releases/latest | grep "tarball_url" | cut -d\" -f4) ; done
+  curl --retry 3 -L -s -o jeedom.tar.gz $DOWNLOAD_URL
+  tar xzf jeedom.tar.gz
+  cd jeedom-core-*
   patch Dockerfile ../Dockerfile.patch
   cat Dockerfile
-  
-  # Workaround for the bug introduced on April 8th
-  echo "[program:apache2]" >> install/OS_specific/Docker/supervisord.conf
-  echo "command=/bin/bash -c \"rm -f /var/run/apache2/apache2.pid; source /etc/apache2/envvars && exec /usr/sbin/apache2 -DFOREGROUND\""  >> install/OS_specific/Docker/supervisord.conf
 
   # Add QEmu to allow the image to be built and tested on x86 processors (especially Travis CI)
   curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz \
